82e3e6272db7882a648f9c0ef74a78ab5b53526e
refandas <refanda10ss@gmail.com>
2019-04-27 20:36:41 +0700

    menambahkan chart pada daftar nilai

--- application/views/daftar_nilai.php
+++ application/views/daftar_nilai.php
@@ -4,6 +4,69 @@
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800"><?= $judul; ?></h1>
 
+          <!-- Chart -->
+          <div style="width: 20%;">
+            <canvas id="myChart" width="1" height="1"></canvas>
+          </div>
+
+          <script>
+            var ctx = document.getElementById('myChart');
+            var myChart = new Chart(ctx, {
+              type: 'bar',
+              data: {
+                labels: ['A', 'B', 'C', 'D', 'E'],
+                datasets: [{
+                  label: '# Jumlah',
+                  data: [
+                    <?php
+                      $a = 0; $b = 0; $c = 0; $d = 0; $e = 0;
+                      foreach($nilai->result() as $val):
+                        if($val->nilai_akhir > 80)
+                          $a++;
+                        else if($val->nilai_akhir > 70 && $val->nilai_akhir <= 80)
+                          $b++;
+                        else if($val->nilai_akhir > 55 && $val->nilai_akhir <= 70)
+                          $c++;
+                        else if($val->nilai_akhir > 35 && $val->nilai_akhir <= 55)
+                          $d++;
+                        else 
+                          $e++;
+                      endforeach;
+                      echo $a . ", " . $b . ", " . $c . ", " . $d . ", " . $e;
+                    ?>
+                  ],
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                  ],
+                  borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                  ],
+                    borderWidth: 1
+                }]
+              },
+              options: {
+                scales: {
+                  yAxes: [{
+                    ticks: {
+                      beginAtZero: true
+                    }
+                  }]
+                }
+              }
+            })
+
+          </script>
+
+          <!-- End of Chart -->
+
           <!-- Content -->
             <table class="table table-striped">
             <thead class="text-center">
@@ -30,11 +93,11 @@
                       <?php
                         if($nilai->nilai_akhir > 80)
                           echo 'A';
-                        else if($nilai->nilai_akhir > 75 && $nilai->nilai_akhir <= 80)
+                        else if($nilai->nilai_akhir > 70 && $nilai->nilai_akhir <= 80)
                           echo 'B';
-                        else if($nilai->nilai_akhir > 60 && $nilai->nilai_akhir <= 75)
+                        else if($nilai->nilai_akhir > 55 && $nilai->nilai_akhir <= 70)
                           echo 'C';
-                        else if($nilai->nilai_akhir > 35 && $nilai->nilai_akhir <= 60)
+                        else if($nilai->nilai_akhir > 35 && $nilai->nilai_akhir <= 55)
                           echo 'D';
                         else 
                           echo 'E';
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,6 +15,7 @@
   <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <script  src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+  <script src="<?= base_url('assets/js/Chart.bundle.js'); ?>"></script>
 
   <!-- Custom styles for this template-->
   <link href="<?= base_url('assets/css/sb-admin-2.css'); ?>" rel="stylesheet">
